c78523c9d16603497476a6c8f042bb0bebc43a1a
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-14 07:48:26 +0000

    Create instantiate_ball function
    This will allow ball feeders to spawn balls

--- PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
+++ PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
@@ -4,8 +4,12 @@ var ball_node = preload("res://SubScenes/Ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	instantiate_ball()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func instantiate_ball():
+	var instance = ball_node.instantiate()
+	add_child(instance)
