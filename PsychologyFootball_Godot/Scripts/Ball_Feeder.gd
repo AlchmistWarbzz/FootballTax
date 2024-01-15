965f7b131b0d091481c9d876799a1b5237bad510
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-15 16:19:26 +0000

    Hook up ball spawning to key input for testing

--- PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
+++ PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
@@ -4,11 +4,17 @@ var ball_node = preload("res://SubScenes/Ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	instantiate_ball($BallSpawnPoint.position)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var ball_to_spawn = false
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		ball_to_spawn = true
+		
+	if ball_to_spawn:
+		instantiate_ball($BallSpawnPoint.position)
 
 func instantiate_ball(pos):
 	var instance = ball_node.instantiate()
