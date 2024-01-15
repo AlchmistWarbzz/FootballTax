54161bbfec46c692f071ec8ad6b5b5e6e598502d
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-15 14:45:18 +0000

    Add parameter to instantiate_ball func

--- PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
+++ PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
@@ -4,12 +4,13 @@ var ball_node = preload("res://SubScenes/Ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	instantiate_ball()
+	instantiate_ball(Vector3(0,0,0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func instantiate_ball():
+func instantiate_ball(pos):
 	var instance = ball_node.instantiate()
+	instance.position = pos
 	add_child(instance)
