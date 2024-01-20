e478a04e204d913952aa50c07765aa1ed13f2adb
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-20 16:08:14 +0000

    Increase ball kick strength

--- PsychologyFootball_Godot/Scripts/Ball.gd
+++ PsychologyFootball_Godot/Scripts/Ball.gd
@@ -9,4 +9,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("spacebar"):
-		apply_central_impulse(Vector3.LEFT * 5)
+		apply_central_impulse(Vector3.LEFT * 10)
