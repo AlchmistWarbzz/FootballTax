25a142b31f4ac8d4cc950038f1bf504a6a1a4fae
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-20 15:05:16 +0000

    Add handling of fixation cone in SST Task Manager

--- PsychologyFootball_Godot/SubScenes/SST_Task_Manager.gd
+++ PsychologyFootball_Godot/SubScenes/SST_Task_Manager.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 var ball_feeder_scene = preload("res://SubScenes/Ball_Feeder.tscn")
 var defender_scene = preload("res://SubScenes/Defender.tscn")
-#var ball_scene = preload("res://SubScenes/Ball.tscn")
+var fixation_cone_scene = preload("res://SubScenes/Fixation_Cone.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,33 +12,60 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("r"):
 		scene_reset()
-			
+	
 	if Input.is_action_just_pressed("t"):
 		scene_setup()
+	
+	if Input.is_action_just_pressed("g"):
+		go_trial_start()
+	
+	if Input.is_action_just_pressed("s"):
+		stop_trial_start()
 
 func scene_reset():
+	# remove left ball feeder
 	if $PlaceholderBallFeederLeft.get_child_count() != 0:
 		$PlaceholderBallFeederLeft/BallFeeder.free()
 	
+	# remove left ball feeder
 	if $PlaceholderBallFeederRight.get_child_count() != 0:
 		$PlaceholderBallFeederRight/BallFeeder.free()
 	
+	# remove fixation cone
+	if $PlaceholderFixationCone.get_child_count() != 0:
+		$PlaceholderFixationCone/FixationCone.free()
+	
+	# remove and respawn left defender
 	if $PlaceholderDefenderLeft.get_child_count() != 0:
 		$PlaceholderDefenderLeft/Defender.free()
+		var new_defender_left = defender_scene.instantiate()
+		$PlaceholderDefenderLeft.add_child(new_defender_left)
 	
+	# remove and respawn left defender
 	if $PlaceholderDefenderRight.get_child_count() != 0:
 		$PlaceholderDefenderRight/Defender.free()
+		var new_defender_right = defender_scene.instantiate()
+		$PlaceholderDefenderRight.add_child(new_defender_right)
 
 func scene_setup():
+	# spawn new ball feeder, randomly choosing left or right side
 	var new_ball_feeder = ball_feeder_scene.instantiate()
 	if randf() > 0.5:
 		$PlaceholderBallFeederLeft.add_child(new_ball_feeder)
 	else:
 		$PlaceholderBallFeederRight.add_child(new_ball_feeder)
 	
-	var new_defender_left = defender_scene.instantiate()
-	$PlaceholderDefenderLeft.add_child(new_defender_left)
-	
-	var new_defender_right = defender_scene.instantiate()
-	$PlaceholderDefenderRight.add_child(new_defender_right)
+	# spawn fixation cone
+	var new_fixation_cone = fixation_cone_scene.instantiate()
+	$PlaceholderFixationCone.add_child(new_fixation_cone)
+
+func go_trial_start():
+	# remove fixation cone
+	if $PlaceholderFixationCone.get_child_count() != 0:
+		$PlaceholderFixationCone/FixationCone.free()
+
+func stop_trial_start():
+	# remove fixation cone
+	if $PlaceholderFixationCone.get_child_count() != 0:
+		$PlaceholderFixationCone/FixationCone.free()
 
