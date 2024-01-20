a713b3bfcc83a4526e6bc0f1c1a1a4a5b85f1dac
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-20 12:29:46 +0000

    prototype defender movement for stop trials (bound to Enter key)

--- PsychologyFootball_Godot/SubScenes/Defender.gd
+++ PsychologyFootball_Godot/SubScenes/Defender.gd
@@ -7,25 +7,34 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var moveTriggerFlag = false;
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		moveTriggerFlag = true
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = get_global_transform().basis.z.normalized()
+	if moveTriggerFlag:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
+	if not is_on_floor_only():
+		velocity.x = 0
+		velocity.z = 0
+
+
