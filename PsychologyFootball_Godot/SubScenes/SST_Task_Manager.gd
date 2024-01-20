ae37fcd752bff2a5696a9c15a8b5c2f8fce698a3
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-20 13:21:18 +0000

    Refactor scene reset and setup into local functions

--- PsychologyFootball_Godot/SubScenes/SST_Task_Manager.gd
+++ PsychologyFootball_Godot/SubScenes/SST_Task_Manager.gd
@@ -8,20 +8,24 @@ var defender_scene = preload("res://SubScenes/Defender.tscn")
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# scene reset
 	if Input.is_action_just_pressed("r"):
-		if $PlaceholderBallFeederLeft.get_child_count() != 0:
-			$PlaceholderBallFeederLeft/BallFeeder.free()
-			
-		if $PlaceholderBallFeederRight.get_child_count() != 0:
-			$PlaceholderBallFeederRight/BallFeeder.free()
+		scene_reset()
 			
 	if Input.is_action_just_pressed("t"):
-		var new_ball_feeder = ball_feeder_scene.instantiate()
-		if randf() > 0.5:
-			$PlaceholderBallFeederLeft.add_child(new_ball_feeder)
-		else:
-			$PlaceholderBallFeederRight.add_child(new_ball_feeder)
+		scene_setup()
+
+func scene_reset():
+	if $PlaceholderBallFeederLeft.get_child_count() != 0:
+		$PlaceholderBallFeederLeft/BallFeeder.free()
+		
+	if $PlaceholderBallFeederRight.get_child_count() != 0:
+		$PlaceholderBallFeederRight/BallFeeder.free()
+
+func scene_setup():
+	var new_ball_feeder = ball_feeder_scene.instantiate()
+	if randf() > 0.5:
+		$PlaceholderBallFeederLeft.add_child(new_ball_feeder)
+	else:
+		$PlaceholderBallFeederRight.add_child(new_ball_feeder)
