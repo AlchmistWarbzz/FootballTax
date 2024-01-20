f2494922958670279dc9a6fb42704b38730be78e
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-20 15:44:15 +0000

    Increase ball speed out of ball feeders

--- PsychologyFootball_Godot/Scripts/Ball.gd
+++ PsychologyFootball_Godot/Scripts/Ball.gd
@@ -3,7 +3,7 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_central_impulse(get_global_transform().basis.z * 7)
+	apply_central_impulse(get_global_transform().basis.z * 14)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
