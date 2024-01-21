95fcf4d23f5657b5d45db65873a6e5b63a60f376
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-21 11:59:48 +0000

    SST Task Manager.gd refactor for clarity

--- PsychologyFootball_Godot/Scripts/SST_Task_Manager.gd
+++ PsychologyFootball_Godot/Scripts/SST_Task_Manager.gd
@@ -6,9 +6,10 @@ var fixation_cone_scene = preload("res://SubScenes/Fixation_Cone.tscn")
 var teammate_scene = preload("res://SubScenes/Teammate.tscn")
 
 var ticks_msec_bookmark = 0
-enum scene_state {WAIT, READY, GO_TRIAL, STOP_TRIAL}
-
 const MSEC_BETWEEN_TRIALS = 3000
+
+enum scene_state {WAIT, READY, GO_TRIAL, STOP_TRIAL}
+var current_state = scene_state.WAIT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,13 +21,13 @@ func _process(delta):
 		scene_reset()
 	
 	if Input.is_action_just_pressed("t"):
-		scene_setup()
+		scene_ready()
 	
 	if Input.is_action_just_pressed("g"):
-		trial_start()
+		trial_start(false)
 	
 	if Input.is_action_just_pressed("s"):
-		trial_start()
+		trial_start(true)
 	
 	# tick-based scene sequencing
 	
@@ -56,7 +57,7 @@ func scene_reset():
 		var new_defender_right = defender_scene.instantiate()
 		$PlaceholderDefenderRight.add_child(new_defender_right)
 
-func scene_setup():
+func scene_ready():
 	# spawn new ball feeder, randomly choosing left or right side
 	var new_ball_feeder = ball_feeder_scene.instantiate()
 	if randf() > 0.5:
@@ -68,7 +69,7 @@ func scene_setup():
 	var new_fixation_cone = fixation_cone_scene.instantiate()
 	$PlaceholderFixation.add_child(new_fixation_cone)
 
-func trial_start():
+func trial_start(is_stop_trial: bool):
 	# remove fixation cone
 	if $PlaceholderFixation.get_child_count() != 0:
 		$PlaceholderFixation/FixationCone.free()
