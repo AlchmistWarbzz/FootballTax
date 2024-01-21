7201823df0efe942bf97e1f5bfb6b14fa8377b50
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-21 13:41:26 +0000

    Improve ball kick realism

--- PsychologyFootball_Godot/Scripts/Ball.gd
+++ PsychologyFootball_Godot/Scripts/Ball.gd
@@ -3,10 +3,11 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_central_impulse(get_global_transform().basis.z * 14)
+	apply_central_impulse(get_global_transform().basis.z * 20)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("spacebar"):
-		apply_central_impulse(Vector3.LEFT * 10)
+		set_linear_velocity(Vector3.ZERO)
+		apply_central_impulse(Vector3.LEFT * 8)
