170aacb96c532acd4db444148cb6e28ba8f219fd
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-22 23:32:37 +0000

    Add signal triggered kicks to Ball.gd

--- PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
+++ PsychologyFootball_Godot/Scripts/Ball_Feeder.gd
@@ -15,6 +15,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# manual keypress sequencing
 	if Input.is_action_just_pressed("g") or Input.is_action_just_pressed("s"):
 		instantiate_ball()
 
