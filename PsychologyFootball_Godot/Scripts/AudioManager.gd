76fd0a90eee6c1ee97ac848df4ee13820d950b1d
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-31 16:11:46 +0000

    Change static type of kick sfx
    Discovered this AudioManager method does not allow for 3D positional audio, but this is fine for the prototype.

--- PsychologyFootball_Godot/Scripts/AudioManager.gd
+++ PsychologyFootball_Godot/Scripts/AudioManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var ambience_sfx: AudioStreamPlayer = $Ambience_SFX
-@onready var football_kick_sfx: AudioStreamPlayer3D = $Football_Kick_SFX
+@onready var football_kick_sfx: AudioStreamPlayer = $Football_Kick_SFX
 
 
 # Called when the node enters the scene tree for the first time.
