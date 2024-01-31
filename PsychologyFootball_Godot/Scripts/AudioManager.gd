97202a63cfa2e2aef0cfbbbf9817cb58afbccb4d
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-01-31 16:17:42 +0000

    Create AudioStreamPlayer for football_touch

--- PsychologyFootball_Godot/Scripts/AudioManager.gd
+++ PsychologyFootball_Godot/Scripts/AudioManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var ambience_sfx: AudioStreamPlayer = $Ambience_SFX
 @onready var football_kick_sfx: AudioStreamPlayer = $Football_Kick_SFX
+@onready var football_touch_sfx: AudioStreamPlayer = $Football_Touch_SFX
 
 
 # Called when the node enters the scene tree for the first time.
