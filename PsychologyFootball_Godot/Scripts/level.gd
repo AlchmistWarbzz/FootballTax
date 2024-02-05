955ba8c82af6294d3b84082d1195e7765c296d7e
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-02-05 00:34:35 +0000

    Handle temporary spawning of individual task manager to main scene
    avoids multiple task managers running together ruining testing

--- PsychologyFootball_Godot/Scripts/level.gd
+++ PsychologyFootball_Godot/Scripts/level.gd
@@ -5,11 +5,15 @@ class_name Level
 @export var level_id : int
 var level_data : LevelData
 
+const SHIFTING_TASK_MANAGER = preload("res://SubScenes/Shifting_Task_Manager.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level_data = LevelManager.get_level_data_by_id(level_id)
 	
+	# temp task manager selection
+	var instance = SHIFTING_TASK_MANAGER.instantiate()
+	add_child(instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
