0ff765d0088f40f9cef7e52d942d30903240518a
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-02-06 09:01:48 +0000

    Add await timer before task starts

--- PsychologyFootball_Godot/Scripts/level.gd
+++ PsychologyFootball_Godot/Scripts/level.gd
@@ -12,9 +12,12 @@ const SST_TASK_MANAGER = preload("res://SubScenes/SST_Task_Manager.tscn")
 func _ready() -> void:
 	level_data = LevelManager.get_level_data_by_id(level_id)
 	
+	await get_tree().create_timer(10.0).timeout
+	
 	# temp task manager selection
 	var instance = SHIFTING_TASK_MANAGER.instantiate()
 	add_child(instance)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
