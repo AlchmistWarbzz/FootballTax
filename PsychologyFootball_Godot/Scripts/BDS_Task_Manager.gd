24b4635e8a47cfa4d2ac2f36bcaabf3c0efaa0c2
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-05-01 03:17:46 +0100

    BDS Fix complete

--- PsychologyFootball_Godot/Scripts/BDS_Task_Manager.gd
+++ PsychologyFootball_Godot/Scripts/BDS_Task_Manager.gd
@@ -4,6 +4,7 @@ extends Node3D
 const BALL_FEEDER_SCENE = preload("res://SubScenes/Ball_Feeder.tscn")
 const FIXATION_CONE = preload("res://SubScenes/Fixation_Cone.tscn")
 const M_TEMP_GOAL = preload("res://Materials/M_TempGoal.tres")
+const BLUE_BALL = preload("res://SubScenes/BlueBall.tscn")
 
 # time
 const TICKS_BETWEEN_TRIALS_MSEC: int = 3000
@@ -26,10 +27,10 @@ var non_shift_trials_per_block: int = NON_SHIFT_TRIALS_PER_PRACTICE_BLOCK
 var block_counter: int = 0
 var trial_counter: int = 0
 var span_length: int = 3
-var shift_trial_counter: int = 0
-var shift_trials_passed: int = 0
-var non_shift_trial_counter: int = 0
-var non_shift_trials_passed: int = 0
+#var shift_trial_counter: int = 0
+var trials_passed: int = 0
+#var non_shift_trial_counter: int = 0
+#var non_shift_trials_passed: int = 0
 
 # metrics
 @onready var metrics_array = Array()
@@ -53,6 +54,7 @@ var is_shift_trial: bool = false
 # spans
 @onready var targets = [$"0/MeshInstance3D", $"1/MeshInstance3D", $"2/MeshInstance3D", $"3/MeshInstance3D", $"4/MeshInstance3D", $"5/MeshInstance3D", $"6/MeshInstance3D"]
 @onready var random_span = Array()
+@onready var random_span_numbers = Array()
 @onready var player_input_span = Array()
 
 # span pointers
@@ -114,8 +116,9 @@ func _process(delta: float) -> void:
 				
 				if not is_trial_passed:
 					#go_trial_failed.emit()
-					print("trial_failed timeout")
-					append_new_metrics_entry(0)
+					print("trial_failed")
+				
+				append_new_metrics_entry()
 				
 				scene_reset()
 				
@@ -131,24 +134,58 @@ func _process(delta: float) -> void:
 				params.to = params.from + get_viewport().get_camera_3d().project_ray_normal(mouse_position) * raycast_length
 				params.collision_mask = 2
 				var result = space_state.intersect_ray(params)
-				if result:
+				if result: #if mouse clicked target
 					print("mouse ray hit target " + result.collider.name)
 					player_input_span.append(result.collider.name)
+					
+					# destroy previous ball if found
+					var old_ball = $Player/PlaceholderBall/Child.get_child(0)
+					if old_ball != null:
+						old_ball.queue_free()
+					
+					# create new ball
+					var instance
+					instance = BLUE_BALL.instantiate()
+					$Player/PlaceholderBall/Child.add_child(instance)
+					ball_kicked.emit(result.collider.get_global_position() + (Vector3.UP * 3))
 				
-				if player_input_span.size() >= random_span.size():
-					print("trial_passed entries given")
-					append_new_metrics_entry(Time.get_ticks_msec() - ticks_msec_bookmark)
+				for n in random_span:
+					var n_stringname = str(targets.find(n))
+					random_span_numbers.append(StringName(n_stringname))
+				
+				if player_input_span == random_span_numbers:
+					# trial passed
+					print("trial passed")
+					is_trial_passed = true
+					trials_passed += 1
+					
+					span_length += 1
+					
+					append_new_metrics_entry()
 					
 					scene_reset()
 					
 					current_state = scene_state.WAIT
 					ticks_msec_bookmark = Time.get_ticks_msec()
+				
+				elif player_input_span.size() >= random_span.size():
+					print("trial failed")
+					
+					append_new_metrics_entry()
+					
+					scene_reset()
+					
+					current_state = scene_state.WAIT
+					ticks_msec_bookmark = Time.get_ticks_msec()
+				
+				random_span_numbers = Array()
+			
 			
 			#elif Input.is_action_just_pressed("kick_left") and not is_trial_passed:
 				#if check_correct_kick(true): # is kick left
 					#ball_kicked.emit($GoalPostLeft.global_position)
 					#is_trial_passed = true
-					#
+					# 
 					#if is_shift_trial:
 						#shift_trials_passed += 1
 						#print("shift_trial_passed")
@@ -216,7 +253,6 @@ func scene_hide_target():
 	random_span[current_target_show_index].set_surface_override_material(0, null)
 
 func scene_trial_start():
-	var bool_string = "shift" if is_shift_trial else "non_shift"
 	print("scene_trial_start")
 	
 	# remove fixation cone
@@ -225,10 +261,6 @@ func scene_trial_start():
 	
 	# update trial counters
 	trial_counter += 1
-	if is_shift_trial:
-		shift_trial_counter += 1
-	else:
-		non_shift_trial_counter += 1
 	
 	# set up flags
 	is_trial_passed = false
@@ -262,8 +294,8 @@ func check_correct_kick(is_kick_left: bool) -> bool:
 		else:
 			return not is_blue_ball
 
-func append_new_metrics_entry(response_time: int):
-	metrics_array.append([block_counter, trial_counter, is_feeder_left, is_blue_ball, is_shift_trial, is_trial_passed, response_time])
+func append_new_metrics_entry():
+	metrics_array.append([block_counter, trial_counter, is_trial_passed])
 
 func write_sst_raw_log(datetime_dict):
 	# open/create file
@@ -281,12 +313,16 @@ func write_sst_raw_log(datetime_dict):
 		raw_log_file.store_line("time: {hour}:{minute}:{second}".format(datetime_dict))
 		raw_log_file.store_line("subject: test") # TODO fill user-input subject and group
 		raw_log_file.store_line("group: test")
-		raw_log_file.store_string("\n-Format Guide-\n\nblock_counter, trial_counter, stimulus_left (ball feeder side), is_blue_ball, is_shift_trial, correct_response, response_time (ms)")
+		raw_log_file.store_string("\n-Format Guide-\n\nblock_counter, trial_counter, correct_response")
 		raw_log_file.store_string("\n\n-Raw Data-\n\n")
 		
 		for sub_array in metrics_array:
-			var line = "{0}, {1}, {2}, {3}, {4}, {5}, {6}"
-			raw_log_file.store_line(line.format(sub_array))
+			#var line = "{0}, {1}, {2}, {3}, {4}, {5}, {6}"
+			#raw_log_file.store_line(line.format(sub_array))
+			for item in sub_array:
+				raw_log_file.store_string(str(item) + ", ")
+			
+			raw_log_file.store_string("\n")
 		
 		raw_log_file.close()
 
@@ -307,41 +343,41 @@ func write_sst_summary_log(datetime_dict):
 		
 		# write counters
 		summary_log_file.store_line("is_practice_block: " + str(is_practice_block))
-		summary_log_file.store_line("non_shift_trials_per_block: " + str(non_shift_trials_per_block))
-		summary_log_file.store_line("shift_trials_per_block: " + str(shift_trials_per_block))
+		#summary_log_file.store_line("non_shift_trials_per_block: " + str(non_shift_trials_per_block))
+		#summary_log_file.store_line("shift_trials_per_block: " + str(shift_trials_per_block))
 		summary_log_file.store_line("block_counter: " + str(block_counter))
 		summary_log_file.store_line("trial_counter: " + str(trial_counter))
-		summary_log_file.store_line("non_shift_trial_counter: " + str(non_shift_trial_counter))
-		summary_log_file.store_line("non_shift_trials_passed: " + str(non_shift_trials_passed))
-		summary_log_file.store_line("shift_trial_counter: " + str(shift_trial_counter))
-		summary_log_file.store_line("shift_trials_passed: " + str(shift_trials_passed))
+		#summary_log_file.store_line("trial_counter: " + str(trial_counter))
+		summary_log_file.store_line("trials_passed: " + str(trials_passed))
+		#summary_log_file.store_line("shift_trial_counter: " + str(shift_trial_counter))
+		#summary_log_file.store_line("shift_trials_passed: " + str(shift_trials_passed))
 		
 		# calculate probability of passing Shift Trials
-		var p_rs: float = float(shift_trials_passed) / float(shift_trial_counter) # successes / total
+		var p_rs: float = float(trials_passed) / float(trial_counter) # successes / total
 		
-		# collect rolling totals for calculating means
-		var rolling_total_shift_reaction_time: int = 0
-		var rolling_total_non_shift_reaction_time: int = 0
+		## collect rolling totals for calculating means
+		#var rolling_total_shift_reaction_time: int = 0
+		#var rolling_total_non_shift_reaction_time: int = 0
 		
-		for sub_array in metrics_array:
-			if sub_array[4] and sub_array[5]:
-				# if shift trial passed
-				rolling_total_shift_reaction_time += sub_array[6]
-			if not sub_array[4] and sub_array[5]:
-				# if non shift trial passed
-				rolling_total_non_shift_reaction_time += sub_array[6]
+		#for sub_array in metrics_array:
+			#if sub_array[4] and sub_array[5]:
+				## if shift trial passed
+				#rolling_total_shift_reaction_time += sub_array[6]
+			#if not sub_array[4] and sub_array[5]:
+				## if non shift trial passed
+				#rolling_total_non_shift_reaction_time += sub_array[6]
 		
-		# calculate mean reaction time (in ms) in Shift trials that were passed
-		var sr_rt = float(rolling_total_shift_reaction_time) / float(shift_trials_passed)
-		
-		# calculate mean reaction time (in ms) in Non Shift trials that were passed
-		var nsr_rt = float(rolling_total_non_shift_reaction_time) / float(non_shift_trials_passed)
+		## calculate mean reaction time (in ms) in Shift trials that were passed
+		#var sr_rt = float(rolling_total_shift_reaction_time) / float(shift_trials_passed)
+		#
+		## calculate mean reaction time (in ms) in Non Shift trials that were passed
+		#var nsr_rt = float(rolling_total_non_shift_reaction_time) / float(non_shift_trials_passed)
 		
 		# write summary data
 		summary_log_file.store_string("\n-Calculated Summary Values-\n\n")
 		summary_log_file.store_line("probability of passing Shift Trials: " + str(p_rs))
-		summary_log_file.store_line("mean reaction time (in ms) in Shift trials that were passed: " + str(sr_rt))
-		summary_log_file.store_line("mean reaction time (in ms) in Non Shift trials that were passed: " + str(nsr_rt))
+		#summary_log_file.store_line("mean reaction time (in ms) in Shift trials that were passed: " + str(sr_rt))
+		#summary_log_file.store_line("mean reaction time (in ms) in Non Shift trials that were passed: " + str(nsr_rt))
 		
 		summary_log_file.close()
 
--- PsychologyFootball_Godot/Scripts/Ball.gd
+++ PsychologyFootball_Godot/Scripts/Ball.gd
@@ -9,7 +9,7 @@ func _ready():
 	#if placeholder_node != null:
 		#task_manager_node = placeholder_node.get_parent()
 	#task_manager_node = $SST_Task_Manager
-	task_manager_node = get_parent().get_parent().get_parent().get_parent() # ...
+	task_manager_node = get_parent().get_parent().get_parent().get_parent()
 	
 	if task_manager_node != null:
 		task_manager_node.ball_kicked.connect(_on_task_manager_ball_kicked)
@@ -32,7 +32,7 @@ func kick(target: Vector3) -> void:
 	set_angular_velocity(Vector3.ZERO)
 	look_at(target)
 	
-	apply_central_impulse(get_global_transform().basis.z * -5)
+	apply_central_impulse(get_global_transform().basis.z * -6)
 	
 	AudioManager.football_kick_sfx.set_pitch_scale(1.0 + (randf() / 20.0))
 	AudioManager.football_kick_sfx.play()
