2682b92cb2da5183eac96477e637de38bc144042
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-05-21 02:57:49 +0100

    Method refactors

--- PsychologyFootball_Godot/Scripts/LevelManager.gd
+++ PsychologyFootball_Godot/Scripts/LevelManager.gd
@@ -6,11 +6,23 @@ var main_scene : Node3D = null
 var loaded_level : Level = null
 var task_to_load_UI
 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
 func unload_level() -> void:
 	if is_instance_valid(loaded_level):
 		loaded_level.queue_free()
 		
 	loaded_level = null 
+
 
 func load_level(level_id : int, task_to_load) -> void:
 	print ("Loading Level: %s" % level_id)
@@ -37,24 +49,15 @@ func load_level(level_id : int, task_to_load) -> void:
 				loaded_level.task_to_load = loaded_level.SHIFTING_TASK_MANAGER
 			3:
 				loaded_level.task_to_load = loaded_level.BDS_TASK_MANAGER
-			
 	else:
 		print ("Level does not exist")
-	
+
+
 func get_level_data_by_id(id : int) -> LevelData:
 	var level_to_return : LevelData = null
 	
 	for lvl : LevelData in Levels:
 		if lvl.level_id == id:
 			level_to_return = lvl
-			
+	
 	return level_to_return
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
